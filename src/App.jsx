8aa2d05c8f1015c127481c0d00481481967e5a65
ali-hamas <alihamasghurki4@gmail.com>
2024-03-07 19:45:53 +0500

    made responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ function App() {
   useEffect(() => {
     let todoString = localStorage.getItem("todos");
     if (todoString) {
-      let todos = JSON.parse(localStorage.getItem("todos"));
-      setAllTodos(todos);
+      setAllTodos(JSON.parse(todoString));
     }
   }, []);
 
@@ -23,6 +22,7 @@ function App() {
     e.preventDefault();
     setAllTodos([...AllTodos, { id: uuidv4(), FormText, isCompleted: false }]);
     setFormText("");
+    console.log(AllTodos);
     saveToLocalStorage();
   };
 
@@ -62,7 +62,7 @@ function App() {
   return (
     <>
       <div className="body-div flex h-screen w-full items-center justify-center">
-        <div className="flex max-h-[80vh] w-[90%] flex-col gap-5 overflow-y-auto overflow-x-hidden rounded-xl bg-white p-5 py-5 font-bold shadow-md sm:w-[500px] sm:px-8">
+        <div className="flex max-h-[80vh] w-[90%] flex-col gap-5 overflow-y-auto overflow-x-hidden rounded-xl bg-white p-5 py-5 font-bold shadow-md md:w-[500px] md:px-8">
           <h1 className="text-2xl text-slate-800">ToDo List</h1>
           <form className="form flex gap-3" onSubmit={handleSubmit}>
             <input
@@ -70,12 +70,12 @@ function App() {
               placeholder="Add your todo"
               value={FormText}
               onChange={handleFormChange}
-              className="w-full rounded-md border border-slate-400 bg-transparent font-normal text-slate-800 placeholder:text-slate-400 sm:border-slate-600"
+              className="w-full rounded-md border border-slate-400 bg-transparent font-normal text-slate-800 placeholder:text-slate-400 md:border-slate-600"
               required
             />
             <button
               type="submit"
-              className="hidden sm:block sm:w-24 sm:rounded-md sm:bg-indigo-600 sm:text-white sm:transition-colors sm:hover:bg-indigo-700"
+              className="hidden md:block md:w-24 md:rounded-md md:bg-indigo-600 md:text-white md:transition-colors md:hover:bg-indigo-700"
             >
               Add
             </button>
